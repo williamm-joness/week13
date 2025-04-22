28e20d55c01fc25787a427d5dd0f1eaa7cd3b044
William Jones <benny@Williams-Macbook-Pro.local>
2025-04-22 18:51:12 -0400

    Level 2 Complete

---
+++ level_two.py
@@ -0,0 +1,7 @@
+sequence = ["hello", 2, 7.1, True]
+print(sequence)
+
+playlist_for_coding = ["feel U luv Me (Knock2)", "No Sympathy (Nimino)", "cLOUDS (J Cole)", "Power (Interupt)", "Tacobel Canon (Ratatat)"]
+print (playlist_for_coding[3])
+
+playlist_for_coding.append("Still Fading (Swimming Paul)")
